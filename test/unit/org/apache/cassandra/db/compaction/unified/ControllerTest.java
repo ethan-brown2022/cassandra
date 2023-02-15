f63fe481a09e291c26e90277110086ab1b1e1ccb
Ethan Brown <ethan.brown@datastax.com>
2023-02-15 11:24:47 -0800

    removed unnecessary test

--- test/unit/org/apache/cassandra/db/compaction/unified/ControllerTest.java
+++ test/unit/org/apache/cassandra/db/compaction/unified/ControllerTest.java
@@ -187,11 +187,6 @@ public abstract class ControllerTest
         controller.startup(strategy, executorService);
     }
 
-    void testFromCompactionStrategyOptions()
-    {
-
-    }
-
     void testValidateCompactionStrategyOptions()
     {
         Map<String, String> options = new HashMap<>();
